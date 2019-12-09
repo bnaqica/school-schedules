7dd9d8dce274da24498f359b90f4985e919cf3c9
Brian Naqica <bnaqica@yahoo.com>
2019-12-08 20:25:20 -0700

    Initial skeleton commit

---
+++ src/test/java/com/bnaqica/schoolschedules/SchoolSchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
